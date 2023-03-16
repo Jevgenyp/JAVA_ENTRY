1b73242cd387570ee24a520f6628c0e2a72e768c
Jevgenijs Pitkevics <Jpcoatingss@gmail.com>
2023-03-16 21:11:52 +0100

    lesson4Homework

--- homework3_4/h4_2.java
+++ homework3_4/h4_2.java
@@ -1,3 +1,65 @@
+//Реализуйте очередь с помощью LinkedList со следующими методами:
+//• enqueue() — помещает элемент в конец очереди,
+//• dequeue() — возвращает первый элемент из очереди и удаляет его,
+//• first() — возвращает первый элемент из очереди, не удаляя.
+
+    import java.util.LinkedList;
+    import java.util.LinkedList;
+    import java.util.Queue;
+
 public class h4_2 {
+    LinkedList<Integer> list;
+
+    public LinkedList<Integer> Queue() {
+        list = new LinkedList<Integer>();
+        return list;
+        
+    }
+
+    public void enqueue(int data) {
+        list.addLast(data);
+    }
+
+    public int dequeue() {
+        if (list.isEmpty()) {
+            throw new RuntimeException("Queue is empty");
+        }
+        return list.removeFirst();
+    }
+
+    public int first() {
+        if (list.isEmpty()) {
+            throw new RuntimeException("Queue is empty");
+        }
+        return list.getFirst();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    
+    
+    
+        public static void main(String[] args) {
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(1);
+            queue.add(2);
+            queue.add(3);
+            System.out.println(queue.peek()); // 1
+            System.out.println(queue.remove()); // 1
+            System.out.println(queue.remove()); // 2
+            System.out.println(queue.remove()); // 3
+            System.out.println(queue.isEmpty()); // true
+        }
+    
+    
     
 }
+
+    
+
--- homework3_4/h4_3.java
+++ homework3_4/h4_3.java
@@ -1,3 +1,68 @@
+//В калькулятор добавьте возможность отменить последнюю операцию.
+
+   
+// К калькулятору из предыдущего дз добавить логирование.
+import java.util.Scanner;
+import java.util.Stack;
+
 public class h4_3 {
-    
+    private static Scanner scanner = new Scanner(System.in);
+    private static Stack<Double> stack = new Stack<>();
+    private static double lastResult = 0.0;
+
+    public static void main(String[] args) {
+        while (true) {
+            
+            System.out.print("Введите первое число: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.print("Введите второе число: ");
+            double num2 = scanner.nextDouble();
+
+            System.out.print("Введите операцию (+, -, *, /) или введите u чтобы удалить предведущую операцию: ");
+            char operator = scanner.next().charAt(0);
+
+            switch (operator) {
+                case '+':
+                    lastResult = num1 + num2;
+                    stack.push(lastResult);
+                    break;
+                case '-':
+                    lastResult = num1 - num2;
+                    stack.push(lastResult);
+                    break;
+                case '*':
+                    lastResult = num1 * num2;
+                    stack.push(lastResult);
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("Деление на ноль невозможно!");
+                        continue;
+                    }
+                    lastResult = num1 / num2;
+                    stack.push(lastResult);
+                    break;
+                case 'u':
+                    if (stack.empty()) {
+                        System.out.println("Стек пуст, невозможно выполнить операцию!");
+                        continue;
+                    }
+                    stack.pop();
+                    if (stack.empty()) {
+                        System.out.println("Стек пуст!");
+                        continue;
+                    }
+                    lastResult = stack.peek();
+                    break;
+                default:
+                    System.out.println("Неверный символ операции!");
+                    continue;
+            }
+
+            System.out.println("Результат: " + lastResult);
+        }
+    }
 }
+
+
